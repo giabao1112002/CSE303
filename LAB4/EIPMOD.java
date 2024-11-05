fd86e1171d34d93bccd32f388f37a200f5e9ff96
Minh-12345 <minh.doduy.cit22@eiu.edu.vn>
2024-11-05 19:21:28 +0700

    5/11

---
+++ LAB4/EIPMOD.java
@@ -0,0 +1,69 @@
+package LAB4;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class EIPMOD {
+    static StringBuilder sb = new StringBuilder();
+    static InputReader reader = new InputReader(System.in);
+
+    public static void main(String[] args) {
+        int x = reader.nextInt();
+        int n = reader.nextInt();
+        int k = reader.nextInt();
+        double result = Math.pow(n, k) % k;
+        System.out.println(result);
+    }
+
+    static class InputReader {
+        StringTokenizer tokenizer;
+        BufferedReader reader;
+        String token;
+        String temp;
+
+        public InputReader(InputStream stream) {
+            tokenizer = null;
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public InputReader(FileInputStream stream) {
+            tokenizer = null;
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String nextLine() throws IOException {
+            return reader.readLine();
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    if (temp != null) {
+                        tokenizer = new StringTokenizer(temp);
+                        temp = null;
+                    } else {
+                        tokenizer = new StringTokenizer(reader.readLine());
+                    }
+                } catch (IOException e) {
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
