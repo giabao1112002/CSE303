4d74945b881c13b367c0eb03a7ee1130f14a01c4
Pham Gia Bao <phamgiabao1112002@gmail.com>
2024-11-12 19:02:23 +0700

    12/11/2024_2

---
+++ LAB4/EISUBSET2 .java
@@ -0,0 +1,71 @@
+package LAB4;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class EISUBSET2 {
+    static StringBuilder sb = new StringBuilder();
+    static InputReader reader = new InputReader(System.in);
+
+    public static void main(String[] args) {
+        int n=reader.nextInt();
+        int k=reader.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=reader.nextInt();
+            
+        }
+    }
+
+    static class InputReader {
+        StringTokenizer tokenizer;
+        BufferedReader reader;
+        String token;
+        String temp;
+
+        public InputReader(InputStream stream) {
+            tokenizer = null;
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public InputReader(FileInputStream stream) {
+            tokenizer = null;
+            reader = new BufferedReader(new InputStreamReader(stream));
+        }
+
+        public String nextLine() throws IOException {
+            return reader.readLine();
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    if (temp != null) {
+                        tokenizer = new StringTokenizer(temp);
+                        temp = null;
+                    } else {
+                        tokenizer = new StringTokenizer(reader.readLine());
+                    }
+                } catch (IOException e) {
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
